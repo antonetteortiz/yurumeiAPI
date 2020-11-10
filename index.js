6cd9d9ef2cb1ac2be0f9b4104f665d94d0eb5546
Antonette_Ortiz <ORTIZANTONETTE@GMAIL.COM>
2020-11-09 21:35:20 -0500

    updates to routers

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const Router = express();
-const Music = require("./models/music")
+const Music = require("./models/music");
 const parser = require("body-parser");
 const { request, response } = require("express");
 
@@ -9,8 +9,17 @@ Router.use(cors());
 Router.use(parser.json());
 
 Router.get("/", (request, response) => {
-    console.log("yerr")
+  console.log("yerr");
   response.redirect("/Music");
+});
+
+Router.get("/Music/:artistName", (request, response) => {
+  Music.find({ artistName: decodeURI(request.params.artistName) }).then(
+    (Music) => {
+      console.log(decodeURI(request.params.artistName));
+      response.json(Music);
+    }
+  );
 });
 
 Router.get("/Music", (request, response) => {
@@ -25,23 +34,27 @@ Router.post("/Music", (request, response) => {
   });
 });
 
-Router.put("/Music/:artist", (request, response) =>{
-  Music.findOneAndUpdate({artistName: request.params.artist}, request.body, {new:true}).then((Music)=>
-  response.json(Music))
-})
+Router.put("/Music/:artist", (request, response) => {
+  Music.findOneAndUpdate(
+    { artistName: decodeURI(request.params.artist) },
+    request.body,
+    { new: true }
+  ).then((Music) => response.json(Music));
+});
 
 Router.delete("/Music/:artist", (request, response) => {
-    Music.findOneAndDelete({ artistName: request.params.artist }).then((Music) => {
-      response.json(Music);
-    });
-  })
+  Music.findOneAndDelete({
+    artistName: decodeURI(request.params.artist),
+  }).then((Music) => {
+    response.json(Music);
+  });
+});
 
 Router.set("port", process.env.PORT || 8080);
 
 Router.listen(Router.get("port"), () => {
   console.log(`✅ PORT: ${Router.get("port")} 🌟`);
 });
-
 
 // Router.listen(9000, () => {
 //   console.log("Garifuna Nuguya");
