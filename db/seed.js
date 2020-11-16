7f827db93cf44525228c7b7a1f82ff95e92d6306
Antonette_Ortiz <ORTIZANTONETTE@GMAIL.COM>
2020-11-16 15:22:26 -0500

    updates

--- db/seed.js
+++ db/seed.js
@@ -1,9 +1,7 @@
 const Music = require("../models/music");
 const MusicData = require("./music.json");
 
-Music.deleteMany({}).then(() =>{
-    console.log("heyy")
-    Music.create(MusicData).then((res) =>
-    console.log(res))
-})
-
+Music.deleteMany({}).then(() => {
+  // console.log("heyy")
+  Music.create(MusicData).then((res) => console.log(res));
+});
