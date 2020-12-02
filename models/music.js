9af972cdc99b7dc889fcabd00b29820efc810884
Antonette_Ortiz <ORTIZANTONETTE@GMAIL.COM>
2020-12-02 11:23:41 -0500

    updated model and json file

--- models/music.js
+++ models/music.js
@@ -4,10 +4,8 @@ const musicSchema = new mongoose.Schema({
   artistName: String,
   title: String,
   genre: String,
-  label: String,
-  producer: String,
   year: String,
-  tracks: [String],
+  link: String,
   artwork: String
 });
 
