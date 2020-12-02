bb3d6870f573d388a76dc6c5d75af9a5d4da90a5
Antonette_Ortiz <ORTIZANTONETTE@GMAIL.COM>
2020-12-02 12:56:57 -0500

    added foldersand recipe model

---
+++ models/recipe.js
@@ -0,0 +1,12 @@
+const mongoose = require("../db/connection");
+
+const recipeSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  photo: String,
+  servings: String,
+  ingredients: [String],
+  steps: [String],
+});
+
+module.exports = mongoose.model("Recipe", recipeSchema);
