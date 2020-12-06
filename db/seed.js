706719b5ea1f0afc6b932fe4c986656afed3ad88
Antonette_Ortiz <ORTIZANTONETTE@GMAIL.COM>
2020-12-05 19:58:46 -0500

    added recipe to seed.js file

--- db/seed.js
+++ db/seed.js
@@ -1,7 +1,15 @@
 const Music = require("../models/music");
 const MusicData = require("./music.json");
 
+const Recipe = require("../models/recipe");
+const RecipeData = require("./recipe.json")
+
 Music.deleteMany({}).then(() => {
-  // console.log("heyy")
+  // console.log("music albums")
   Music.create(MusicData).then((res) => console.log(res));
+});
+
+Recipe.deleteMany({}).then(() => {
+  // console.log("what's cooking")
+  Recipe.create(RecipeData).then((res) => console.log(res));
 });
